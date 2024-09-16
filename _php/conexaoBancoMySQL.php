704d5ad00459a9a7eba2b279819e6f8b26dc1953
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-09-16 10:30:41 -0300

    Create conexaoBancoMySQL.php
    Script para conexão com o banco de dados MySQL

---
+++ _php/conexaoBancoMySQL.php
@@ -0,0 +1,14 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $bancoDeDados = "recicla_ai";
+
+    $con = new mysqli($servidor, $usuario, $senha, $bancoDeDados);
+
+    if($con->connect_error) {
+        exit("Falha na conexão: $con->connect_error");
+    }
+    echo "Sucesso na conexão com o banco de dados";
+
+?>
