113bd1cb747bb447711015354b1b3c1c023ae355
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-09-24 11:19:41 -0300

    Update editar_cadastro.php

--- _php/editar_cadastro.php
+++ _php/editar_cadastro.php
@@ -1,60 +1,105 @@
 <?php
 require_once('../_php/dados_sessao.php'); // Inclui a função de conexão
 
+// Conectar ao banco de dados
+$con = getConexaoBancoMySQL();
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Conectar ao banco de dados
-$con = getConexaoBancoMySQL();
-
 // Atualizar dados se o formulário for enviado
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    $senha = $_POST['senha']; // Você pode optar por fazer hash da senha antes de salvar
+    $senhaAtual = $_POST['senhaAtual'];
+    $senhaNova = $_POST['senhaNova'];
 }
 
 // Atualiza usuário se o botão Atualizar for clicado
 if (isset($_POST['atualizar'])) {
-    $stmt = $con->prepare("UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?");
-    
-    // Armazena o hash da senha em uma variável para não dar problema de notice:
-    $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-    
-    $stmt->bind_param("sssi", $nome, $email, $senhaHash, $_SESSION['usuario_id']);
-    
-    if ($stmt->execute()) {
-        echo "<script>alert('Dados atualizados com sucesso!');
-        window.location.href = '../_public/sessao.php';
-        </script>";
+
+    $stmt1 = $con->prepare("SELECT * FROM usuarios WHERE email = ?");
+    $stmt1->bind_param("s", $email);
+    $stmt1->execute();
+    $resultado = $stmt1->get_result();
+
+    $usuario = $resultado->fetch_assoc();
+
+    if (password_verify($senhaAtual, $usuario['senha'])) {
+
+        $stmt1->close();
+
+        $stmt2 = $con->prepare("UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?");
+        // Armazena o hash da senha em uma variável para não dar problema de notice:
+        $senhaHash = password_hash($senhaNova, PASSWORD_DEFAULT);
+            
+        $stmt2->bind_param("sssi", $nome, $email, $senhaHash, $_SESSION['usuario_id']);
+            
+        if ($stmt2->execute()) {
+            $stmt2->close();
+            echo "<script>alert('Dados atualizados com sucesso!');
+            window.location.href = '../_public/sessao.php';
+            </script>";
+                
+        } else {
+            $stmt2->close();
+            echo "<script>alert('Erro ao atualizar dados.');
+            window.location.href = '../_public/sessao.php';
+            </script>";
+        }
         
-    } else {
-        echo "<script>alert('Erro ao atualizar dados.');
-        window.location.href = '../_public/sessao.php';
+    }
+    else {
+        $stmt1->close();
+        echo "<script>
+        alert('Senha atual incorreta.');
+        window.location.href = '../_public/sessao.php'; // Redireciona para a mesma página
         </script>";
     }
-    $stmt->close();
 }
 
 
 // Excluir usuário se o botão Excluir for clicado
 if (isset($_POST['excluir'])) {
-    $stmt = $con->prepare("DELETE FROM usuarios WHERE id = ?");
-    $stmt->bind_param("i", $_SESSION['usuario_id']);
-    if ($stmt->execute()) {
-        session_destroy(); // Sai da sessão
+
+    $stmt1 = $con->prepare("SELECT * FROM usuarios WHERE email = ?");
+    $stmt1->bind_param("s", $email);
+    $stmt1->execute();
+    $resultado = $stmt1->get_result();
+
+    $usuario = $resultado->fetch_assoc();
+    if (password_verify($senhaAtual, $usuario['senha'])) {
+
+        $stmt1->close();
+
+        $stmt2 = $con->prepare("DELETE FROM usuarios WHERE id = ?");
+        $stmt2->bind_param("i", $_SESSION['usuario_id']);
+
+        if ($stmt2->execute()) {
+            session_destroy(); // Sai da sessão
+            $stmt2->close();
+            echo "<script>
+                    alert('Usuário excluído com sucesso. Entre com outro usuário ou cadastre-se novamente.');
+                    window.location.href = '../_public/entrar.html';
+                    </script>";
+            exit();
+        } else {
+            $stmt2->close();
+            echo "<script>alert('Erro ao excluir usuário.');
+            window.location.href = '../_public/sessao.php';
+            </script>";
+        }
+    }
+    else {
+        $stmt1->close();
         echo "<script>
-                alert('Usuário excluído com sucesso. Entre com outro usuário ou cadastre-se novamente.');
-                window.location.href = '../_public/entrar.html';
-                </script>";
-        exit();
-    } else {
-        echo "<script>alert('Erro ao excluir usuário.');
-        window.location.href = '../_public/sessao.php';
+        alert('Senha atual incorreta.');
+        window.location.href = '../_public/sessao.php'; // Redireciona para a mesma página
         </script>";
     }
-    $stmt->close();
+
+    
 }
 
 // Código para sair da sessão ao clicar em "Sair da Conta"
