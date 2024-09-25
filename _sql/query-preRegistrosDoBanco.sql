d8bbc0c37d7c2c7d1bafbdb04d62c5db145d59ba
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-09-25 11:04:31 -0300

    Update query-preRegistrosDoBanco.sql

---
+++ _sql/query-preRegistrosDoBanco.sql
@@ -0,0 +1,7 @@
+INSERT INTO TipoUsuario VALUES
+    (1, 'Comum'),
+    (2, 'Admin');
+
+INSERT INTO Usuario VALUES
+    (null, 1, 'user', '$2y$12$L4ePJnjek./3GG0SYz1KeudIfgrMmHiZ8n2uk0lGdcEKm660vcVYu', 'Usuário de Teste', 'user@email.com'),
+    (null, 2, 'admin', '$2y$12$imw0l38FzRQd6eHwlw7YXO7LiQJxKxtmbHQQp/FMWBIfdWwV3JGBK', 'Admin de Teste', 'admin@email.com');
