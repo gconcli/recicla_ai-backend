69f798e5f954e2b846e751b7f071be7280812749
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-09-25 10:42:23 -0300

    Update novo_cadastro.php

--- _php/novo_cadastro.php
+++ _php/novo_cadastro.php
@@ -2,12 +2,29 @@
     require_once 'scripts.php';
     require_once 'classes.php';
 
-    $idTipo = 1;
     $login = $_POST['login'];
-    $senhaAtual = $_POST['senhaAtual'];
+    $senha = $_POST['senha'];
     $nome = $_POST['nome'];
     $email = $_POST['email'];
 
-    if(session_status() !== PHP_SESSION_ACTIVE)
-        session_start();
+    $usuarioVO = new UsuarioVO();
+    $usuarioVO->setIdTipoUsuario(1);
+    $usuarioVO->setLogin($login);
+    $usuarioVO->setSenha($senha);
+    $usuarioVO->setNome($nome);
+    $usuarioVO->setEmail($email);
+
+    $tentativaCadastro = FactoryServicos::getServicosUsuario()->cadastroUsuario($usuarioVO);
+
+    if(empty($tentativaCadastro)) {
+        echo"<script>
+                alert('Erro no cadastro!');
+                window.location.href = '../index.html';
+            </script>";
+    }
+    else {
+        fazerLogin($login, $senha);
+    }
+
+    session_start();
 ?>
