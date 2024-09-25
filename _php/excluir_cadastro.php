f02e74be1ed2124d7525a0cde7258735a6e05108
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-09-25 10:42:18 -0300

    Update excluir_cadastro.php

--- _php/excluir_cadastro.php
+++ _php/excluir_cadastro.php
@@ -2,7 +2,31 @@
     require_once 'scripts.php';
     require_once 'classes.php';
 
+    $login = $_POST['login'];
+    $senha = $_POST['senha'];
 
+    if(password_verify($senhaAtual, $_SESSION[UsuarioVO::getNomesColunasTabela[3]])) {
 
-    logoff();
+        $tentativaRemocao = FactoryServicos::getServicosUsuario()->deletarUsuario($_SESSION[UsuarioVO::getNomesColunasTabela[0]]);
+
+        if(empty($tentativaRemocao)) {
+            echo"<script>
+                    alert('Erro na remoção!');
+                    window.location.href = '../index.html';
+                </script>";
+        }
+        else {
+            echo"<script>
+                    alert('Cadastro removido com sucesso! Faça login novamente...');
+                    window.location.href = '../index.html';
+                </script>";
+                fazerLogoff();
+        }
+    }
+    else {
+        echo"<script>
+                alert('Senha incorreta!');
+                window.location.href = '../index.html';
+            </script>";
+    }
 ?>
