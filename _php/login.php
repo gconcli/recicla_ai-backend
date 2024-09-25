f5f4f708190722bf8914f62f4155cfb7bc178301
Eduardo Pereira Moreira <dudu.pm@hotmail.com>
2024-09-25 10:42:21 -0300

    Update login.php

--- _php/login.php
+++ _php/login.php
@@ -5,35 +5,5 @@
     $login = $_POST['login'];
     $senhaAtual = $_POST['senhaAtual'];
 
-    $tentativaLogin = FactoryServicos::getServicosUsuario()->loginUsuario($login, $senhaAtual);
-
-    if(isset($tentativaLogin)) {
-        if(!$tentativaLogin) {
-            echo"<script>
-                alert('Senha incorreta!');
-                window.location.href = '../index.html';
-            </script>";
-        }
-        else {
-
-            session_start();
-            $_SESSION['id'] = $tentativaLogin->getId();
-            $_SESSION['idTipo'] = $tentativaLogin->getIdTipoUsuario();
-            $_SESSION['login'] = $tentativaLogin->getLogin();
-            $_SESSION['senha'] = $tentativaLogin->getSenha();
-            $_SESSION['nome'] = $tentativaLogin->getNome();
-            $_SESSION['email'] = $tentativaLogin->getEmail();
-
-            echo"<script>
-                alert('Dados corretos, fazendo login...');
-                window.location.href = '../index.html';
-            </script>";
-        }
-    }
-    else {
-        echo"<script>
-                alert('Usuário não encontrado!');
-                window.location.href = '../index.html';
-            </script>";
-    }
+    fazerLogin($login, $senhaAtual);
 ?>
